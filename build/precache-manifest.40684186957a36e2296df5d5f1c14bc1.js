013f32da1188aea7f969c5b777de18a03df841b6
ajeet0804 <ajeet20lodhi@gmail.com>
2020-10-11 15:16:58 +0530

    test

---
+++ build/precache-manifest.40684186957a36e2296df5d5f1c14bc1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d180e254b232a334b50a5ef7803bf48e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4a06b0283381f9d506ca",
+    "url": "/static/css/2.1a660e5d.chunk.css"
+  },
+  {
+    "revision": "2540fcdb63e79786af67",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "4a06b0283381f9d506ca",
+    "url": "/static/js/2.64433057.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.64433057.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2540fcdb63e79786af67",
+    "url": "/static/js/main.649ddd04.chunk.js"
+  },
+  {
+    "revision": "effa775c1c0be15ade75",
+    "url": "/static/js/runtime-main.951173b6.js"
+  }
+]);
